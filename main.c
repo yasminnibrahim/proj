2ae5d5ee678bf00eb799189eee1a349c934d71cb
yasminnibrahim <yaseminibrahim64@gmail.com>
2017-12-21 14:40:58 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+int n=0;
+Book books[100];
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+void add_new_book(){
+     Book a
+     a=read_book();
+     books[n]=a;
+     n++;}
+}
+Member registration(Member a){
+    read_member(a);
+    return a;
+    /*data is stored in the file */
+}
+void add_new_copy (){
+    printf("Enter ISBN:\n");
+    scanf("%s",ISBN);
+    do{
+     printf("Enter NOC:\n");
+     scanf("%d",&NOC);}while(NOC<0);
+}
+int search_ISBN(char*ISBN){
+    int k=-1;
+    for(i=0;i<n;i++){
+        if(*ISBN==books[i])
+            return k=1;
+    }
+    return k;
+}
+void Delete_Book(int k){
+ int i;
+ for(i=k;i<n;i++)
+    books[i]=books[i+1]
+ }
+void borrowing (books b){
+     b=read_book;
+     NOC--;
+     if}
+     void search_for_book(){
+
+
+
+
+     }
